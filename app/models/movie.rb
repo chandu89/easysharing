cc9a972e356890c457b2e9dcccb8d7a32a88d3fa
chandanwtb <chandan.choubey@experityhealth.com>
2020-07-18 19:32:54 +0530

    Added logo png created free online

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@
 class Movie < ApplicationRecord
   belongs_to :user
   validates :video_url, presence: true
-  validates :video_url, format: URI::regexp(%w[http https])
+  validates :video_url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 end
