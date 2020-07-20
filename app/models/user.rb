f46dc9609fe98027c9232ad5672a8357ba1c3e55
chandanwtb <chandan.choubey@experityhealth.com>
2020-07-20 17:06:30 +0530

    Mailer added for both env prod and dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # User model is used deal with your user related changes
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :movies, dependent: :destroy
 end
